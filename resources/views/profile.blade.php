168321f81caf3bfba2eda9cda47337bea34bdff8
abbiesdepot <cabigail03@student.ciputra.ac.id>
2025-10-14 02:29:49 +0700

    bam10

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -16,8 +16,16 @@
 <body>
     @include('layout.nav')
 
-    <div class=""></div>
+    <div class="bg-light d-flex justify-content-center align-items-center vh-100">
 
+        <div class="card shadow-lg border-0" style="width: 22rem; border-radius: 1.5rem;">
+            <div class="card-body text-center p-4">
+                <h4 class="card-title mb-1">{{ $user['name'] }}</h4>
+                <p class="text-muted">{{ $user['email'] }}</p>
+            </div>
+        </div>
+
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
