016a9308b2f2e0ea9e1287cf75e080d41984cc45
abbiesdepot <cabigail03@student.ciputra.ac.id>
2026-01-06 06:54:40 +0700

    data + dummy data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -71,6 +71,26 @@ class DatabaseSeeder extends Seeder
             ]
         );
 
+        $product2 = Product::updateOrCreate(
+            ['name' => 'Iris Sculpt'],
+            [
+                'category_id' => $categories->first()->id,
+                'slug' => 'iris-sculpt',
+                'description' => 'Contoured fit with breathable stretch fabric for all-day comfort.',
+                'price' => 189000,
+                'stock_quantity' => 8,
+                'is_featured' => false,
+            ]
+        );
+
+        $product2->images()->updateOrCreate(
+            ['is_primary' => true, 'product_id' => $product2->id],
+            [
+                'image_url' => 'products/irissculpt.png',
+                'order' => 0,
+            ]
+        );
+
         $customer = User::updateOrCreate(
             ['email' => 'customer@test.com'],
             [
@@ -81,29 +101,59 @@ class DatabaseSeeder extends Seeder
             ]
         );
 
-        // if (app()->environment('local')) {
-        //     $customer = User::updateOrCreate(
-        //         ['email' => 'customer@test.com'],
-        //         [
-        //             'name' => 'Test Customer',
-        //             'password' => Hash::make('password'),
-        //             'phone' => '0812-3456-7890',
-        //             'role' => 'customer',
-        //         ]
-        //     );
+        // DUMMY DATA FOR PAGES
+        $productNames = [
+            'Luxe Wool Coat', 'Classic Denim Jacket', 'Silk Blouse', 'Cotton Tee', 
+            'Leather Jacket', 'Cashmere Sweater', 'Linen Shirt', 'Satin Dress',
+            'Cargo Pants', 'Pleated Skirt', 'Wide Leg Trousers', 'Skinny Jeans',
+            'Ankle Boots', 'Canvas Sneakers', 'Leather Loafers', 'Suede Pumps',
+            'Gold Necklace', 'Silver Bracelet', 'Pearl Earrings', 'Diamond Ring',
+            'Leather Bag', 'Crossbody Purse', 'Tote Bag', 'Clutch Wallet',
+        ];
 
-        //     Store::factory()->count(3)->create();
+        foreach ($productNames as $index => $name) {
+            $product = Product::updateOrCreate(
+                ['slug' => Str::slug($name)],
+                [
+                    'name' => $name,
+                    'category_id' => $categories->random()->id,
+                    'description' => 'Premium quality ' . strtolower($name) . ' crafted with attention to detail. Perfect for any occasion.',
+                    'price' => rand(99, 599) * 1000,
+                    'stock_quantity' => rand(0, 20),
+                    'is_featured' => $index < 3,
+                ]
+            );
 
-        //     Product::factory()->count(4)->make()->each(function ($product) use ($categories) {
-        //         $product->category_id = $categories->random()->id;
-        //         $product->save();
+            $product->images()->updateOrCreate(
+                ['is_primary' => true, 'product_id' => $product->id],
+                [
+                    'image_url' => 'https://picsum.photos/seed/' . Str::slug($name) . '/800/800',
+                    'order' => 0,
+                ]
+            );
+        }
 
-        //         $product->images()->create([
-        //             'image_url' => 'https://picsum.photos/seed/' . Str::random(6) . '/800/800',
-        //             'is_primary' => true,
-        //             'order' => 0,
-        //         ]);
-        //     });
-        // }
+        // Generate dummy stores
+        $storeData = [
+            ['name' => 'Plaza Indonesia', 'city' => 'Jakarta', 'address' => 'Jl. MH Thamrin No.28-30'],
+            ['name' => 'Grand Indonesia', 'city' => 'Jakarta', 'address' => 'Jl. MH Thamrin No.1'],
+            ['name' => 'Pacific Place', 'city' => 'Jakarta', 'address' => 'Jl. Jend. Sudirman Kav 52-53'],
+            ['name' => 'Senayan City', 'city' => 'Jakarta', 'address' => 'Jl. Asia Afrika No.19'],
+            ['name' => 'Pondok Indah Mall', 'city' => 'Jakarta', 'address' => 'Jl. Metro Pondok Indah'],
+            ['name' => 'Tunjungan Plaza', 'city' => 'Surabaya', 'address' => 'Jl. Basuki Rahmat No.8-12'],
+            ['name' => 'Galaxy Mall', 'city' => 'Surabaya', 'address' => 'Jl. Dharmahusada Indah Timur'],
+            ['name' => 'Beachwalk Shopping Center', 'city' => 'Bali', 'address' => 'Jl. Pantai Kuta'],
+        ];
+
+        foreach ($storeData as $data) {
+            \App\Models\Store::updateOrCreate(
+                ['name' => $data['name']],
+                [
+                    'address' => $data['address'],
+                    'city' => $data['city'],
+                    'phone' => '021-' . rand(1000000, 9999999),
+                ]
+            );
+        }
     }
 }
